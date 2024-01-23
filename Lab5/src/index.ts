59a4b2d9abf6d0f10e30b431e37f649cfd5c6e7c
alan.yiu <alan.yiu@hk.yusen-logistics.com>
2024-01-23 21:23:15 +0800

    feat: Lab 5

---
+++ Lab5/src/index.ts
@@ -0,0 +1,58 @@
+import Koa from 'koa';
+import Router, { RouterContext } from 'koa-router';
+import logger from 'koa-logger';
+import json from 'koa-json';
+import bodyParser from 'koa-bodyparser';
+import { CustomErrorMessageFunction, query, body, validationResults } from 'koa-req-validation';
+
+const app: Koa = new Koa();
+const router: Router = new Router();
+
+router.get('/', async (ctx: RouterContext, next: any) => {
+  ctx.body = { msg: 'Hello world!' };
+  await next();
+});
+
+const custom404 = async (ctx: RouterContext, next: any) => {
+  try {
+    await next();
+    if (ctx.status === 404) {
+      ctx.status = 404;
+      ctx.body = { err: 'No such endpoint existed' };
+    }
+  } catch (err: any) {
+    ctx.body = { err: err };
+  }
+};
+
+app.use(json());
+app.use(logger());
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
+
+const customErrorMessage: CustomErrorMessageFunction = (_ctx: RouterContext, value: string) => {
+  return (
+    `The name must be between 3 and 20 ` + `characters long but received length ${value.length}`
+  );
+};
+
+const validatorName = [query('name').isLength({ min: 3 }).withMessage(customErrorMessage).build()];
+
+router.get('/', ...validatorName, async (ctx: RouterContext, next: any) => {
+  const result = validationResults(ctx);
+
+  if (result.hasErrors()) {
+    ctx.status = 422;
+    ctx.body = { err: result.mapped() };
+  } else {
+    ctx.body = { msg: `Hello ${ctx.query.name}` };
+  }
+
+  await next();
+});
+
+app.use(custom404);
+
+app.listen(10888, () => {
+  console.log('Koa Started');
+});
